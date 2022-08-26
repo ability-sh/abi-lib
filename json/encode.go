50231c57f2637c4b58411e7f729cc3a196a4ee0d
T-rex <0x50Fc@gmail.com>
2022-08-26 12:52:59 +0800

    Update encode.go

--- json/encode.go
+++ json/encode.go
@@ -190,15 +190,19 @@ func MarshalIndent(object interface{}, prefix, indent string) ([]byte, error) {
 
 	w := bytes.NewBuffer(nil)
 
-	enc := json.NewEncoder(w)
-
-	enc.SetIndent(prefix, indent)
-
-	err := enc.Encode(object)
+	err := encode(object, w)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return w.Bytes(), nil
+	dst := bytes.NewBuffer(nil)
+
+	err = json.Indent(dst, w.Bytes(), prefix, indent)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dst.Bytes(), nil
 }
